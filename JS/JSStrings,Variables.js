34a038a6dfab27067b8111adb796907061c70e82
Vishnu <chippa.vishnu047@gmail.com>
2024-01-10 22:33:05 +0530

    Js Strings

--- JS/JSStrings,Variables.js
+++ JS/JSStrings,Variables.js
@@ -8,3 +8,10 @@ console.log(string2);
 console.log(string3);
 temp=`${string1}${string2}`;
 console.log(temp);
+// to use '' and "" ,use ``(backticks) and also called as dynamically assigning something
+temp=`${string1} is example to use '' and "" within a string`;
+console.log(temp);
+var len=string3.length;
+console.log(`Length of String3 is ${len}`);
+// you can write html code via js also
+document.getElementById('content').innerHTML=' <h1> this is h1 heading </h1> ';
