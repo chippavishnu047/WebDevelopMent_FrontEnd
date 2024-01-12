0a2c0acf9eaee4f787868af71c8aee68ea91cb94
Vishnu <chippa.vishnu047@gmail.com>
2024-01-12 08:40:57 +0530

    String Methods

--- JS/JSStrings,Variables.js
+++ JS/JSStrings,Variables.js
@@ -14,4 +14,17 @@ console.log(temp);
 var len=string3.length;
 console.log(`Length of String3 is ${len}`);
 // you can write html code via js also
-document.getElementById('content').innerHTML=' <h1> this is h1 heading </h1> ';
+// document.getElementById('content').innerHTML=' <h1> this is h1 heading </h1> ';
+//String Mehtods
+var s="String";
+console.log(s);
+//slice,substring method takes parameters as starting index to end index
+console.log(s.substring(1,5));
+console.log(s.slice(1,5));
+//substr method takes parameters as starting index and length of substring 
+console.log(s.substr(1,5));
+
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.charAt(3));
+console.log(s.charCodeAt('A'));
